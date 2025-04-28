c97889e0d88bd5394a3d09e96f5bb2c5e21fb6ec
村头的猫 <ttghost@126.com>
2025-04-28 16:37:20 +0800

    Update preOrder.js

--- js-code/BinaryTree/preOrder.js
+++ js-code/BinaryTree/preOrder.js
@@ -8,24 +8,27 @@ function preOrderEach(root) {
   const result = []
   const loop = (node) => {
     if (node === null) return
+    result.push(node.value)
     if (node.left !== null) preOrderEach(node.left)
     if (node.right !== null) preOrderEach(node.right)
-    result.push(node.value)
+    
   }
+  loop(root)
   return result
 }
 
 // 迭代实现
 function loopPreOrderEach(root) {
-  if (root === null) return
-  const stack = [root]
   const result = []
+  if (root === null) return result
+  const stack = [root]
   while(stack.length) {
     const node = stack.pop()
     result.push(node.value)
     if (node.right !== null) stack.push(node.right)
     if (node.left !== null) stack.push(node.left)
   }
+  return result
 }
 
 /* 构建测试树
@@ -45,4 +48,5 @@ root.right.left = new TreeNode(6);
 root.right.right = new TreeNode(7);
 
 // 输出后序遍历结果：[1,2,4,5,3,6,7]
-console.log(preOrderEach(root)); 
+console.log('前序遍历（递归）：', preOrderEach(root)); 
+console.log('前序遍历（迭代）：', loopPreOrderEach(root)); 
