d7cff2ae2af90cff8646f6a2be091a3c6111799a
村头的猫 <ttghost@126.com>
2025-04-28 17:22:22 +0800

    Update preOrder.js

--- js-code/BinaryTree/preOrder.js
+++ js-code/BinaryTree/preOrder.js
@@ -16,7 +16,7 @@ function preOrder(root) {
   return result
 }
 
-// 迭代实现
+// 迭代实现：单栈法
 function loopPreOrder(root) {
   const result = []
   if (root === null) return result
@@ -28,6 +28,24 @@ function loopPreOrder(root) {
     if (node.left !== null) stack.push(node.left)
   }
   return result
+}
+
+// 迭代实现：统一迭代法
+function loopPreOrder2(root) {
+  const stack = [root], result = [];
+  while (stack.length) {
+    const node = stack.pop();
+    // 双等号判断包括null和undefined
+    if (node == null) continue;
+    if (node instanceof TreeNode) {
+      stack.push(node.right);     // 右
+      stack.push(node.left);      // 左
+      stack.push(node.value);       // 根（标记为已处理）
+    } else {
+      result.push(node);          // 访问值
+    }
+  }
+  return result;
 }
 
 /* 构建测试树
@@ -48,4 +66,5 @@ root.right.right = new TreeNode(7);
 
 // 输出后序遍历结果：[1,2,4,5,3,6,7]
 console.log('前序遍历（递归）：', preOrder(root)); 
-console.log('前序遍历（迭代）：', loopPreOrder(root)); 
+console.log('前序遍历（迭代：单栈法）：', loopPreOrder(root)); 
+console.log('前序遍历（迭代：统一迭代法）：', loopPreOrder2(root)); 
